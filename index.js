9e32858d8a26b194bf4c583c4a1d21191bbf9c49
RohitBehera25 <153441363+RohitBehera25@users.noreply.github.com>
2024-06-21 13:07:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+app.use(cors());
+app.use;
+app.use(express.json());
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "abc123",
+  database: "fb21june24",
+});
+
+app.post("/save", (req, res) => {
+  let data = [req.body.name, req.body.feedback];
+  let sql = "insert into student value(?,?)";
+  con.query(sql, data, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+app.listen(9000, () => {
+  console.log("reaady to serve @9000");
+});
